febb48e0d423df8ed64dbcb27752c324acdab077
Sarah <sarahashih@gmail.com>
2024-11-15 15:54:38 -0800

    minor tweaks to help url and add to cart

--- react-vite/src/components/SmallProductTile/SmallProductTile.jsx
+++ react-vite/src/components/SmallProductTile/SmallProductTile.jsx
@@ -1,4 +1,7 @@
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import { useState } from 'react';
+import { addToCart } from "../../redux/cartReducer";
 import { FaPlus } from 'react-icons/fa'; 
 import { FaUser } from 'react-icons/fa';
 import { FaStar } from 'react-icons/fa';
@@ -6,10 +9,20 @@ import './SmallProductTile.css'
 
 export default function ProductsPage({product}) {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [isAddedToCart, setIsAddedToCart] = useState(false);
 
     const handleTileClick = () => {
         navigate(`/product/${product.id}`)
     }
+
+    const handleAddToCart = (event) => {
+        event.stopPropagation();
+        if (!isAddedToCart) {
+            dispatch(addToCart(product.id));
+            setIsAddedToCart(true);
+        }
+    };
 
     const tileRating = product.average_rating ? (
         <>
@@ -25,8 +38,18 @@ export default function ProductsPage({product}) {
         <p>{tileRating}</p>
     </div>
     <div className="small-product-creator"><FaUser className="small-user-icon" />  {product.username}</div>
-    <div className="small-product-price">${product.price}</div>
-    <button className="small-button-add-cart"><FaPlus className="small-plus-icon" />                                                                                    Add to Cart</button>
+    <div className="small-product-price">${product.price.toFixed(2)}</div>
+    <button 
+        className={`button-add-cart ${isAddedToCart ? 'added-to-cart' : ''}`}
+         onClick={handleAddToCart}
+        disabled={isAddedToCart}
+        >
+            {isAddedToCart ? 'Added to Cart' : (
+                 <>
+                    <FaPlus className="plus-icon" /> Add to Cart
+                </>
+        )}
+    </button>
 </div>
   );
 }
